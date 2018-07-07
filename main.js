f1542fcf5d765e32d2301c393c779f8d50f1a9eb
Perdog <jacob_perry@hotmail.ca>
2018-07-07 17:08:05 -0400

    Theme
    - Changing the way the header is displayed
    - Adding a theme so it's not so fucking gross looking

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 var lastShown;
 var lastIndex;
+var alreadySwitching = false;
 
-$(".corp-card").on("click", function() {
+$(".corp-button").on("click", function() {
+	if (alreadySwitching)
+		return;
+	else
+		alreadySwitching = true;
+	
 	var str = "#" + $(this).attr('toShow');
 	var ind = Number($(this).attr('index'));
 	console.log(str);
@@ -14,10 +20,11 @@ $(".corp-card").on("click", function() {
 		$(lastShown).hide("slide", {direction: (ind > lastIndex ? "left" : "right")}, 500);
 	
 	setTimeout(function() {
-		$(str).show("slide", {direction: (lastIndex ? (ind > lastIndex ? "right" : "left") : "left")}, 500);
-		
-		lastShown = str;
-		lastIndex = ind;
-	}, 510);
+		$(str).show("slide", {direction: (lastIndex ? (ind > lastIndex ? "right" : "left") : "left")}, 500, function() {
+			lastShown = str;
+			lastIndex = ind;
+			alreadySwitching = false;
+		});
+	}, 525);
 	
 });
