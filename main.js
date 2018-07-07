f4cf05ce3234d395d92f4a283ed7285993ad7ba2
Perdog <jacob_perry@hotmail.ca>
2018-07-07 09:57:45 -0400

    Yaaaa slides

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 var lastShown;
+var lastIndex;
 
 $(".corp-card").on("click", function() {
 	var str = "#" + $(this).attr('toShow');
+	var ind = Number($(this).attr('index'));
 	console.log(str);
+	console.log(ind);
 	
 	if (lastShown == str)
 		return;
 	
 	if (lastShown)
-		$(lastShown).slideUp(1000);
-	$(str).slideDown(1000);
+		$(lastShown).hide("slide", {direction: (ind > lastIndex ? "left" : "right")}, 500);
 	
-	lastShown = str;
+	setTimeout(function() {
+		$(str).show("slide", {direction: (lastIndex ? (ind > lastIndex ? "right" : "left") : "left")}, 500);
+		
+		lastShown = str;
+		lastIndex = ind;
+	}, 510);
+	
 });
