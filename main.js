d3966fd64a36d56851d2afcc26b2b4dbdfa26c97
Perdog <jacob_perry@hotmail.ca>
2018-07-07 18:18:15 -0400

    Whoops....

--- main.js
+++ main.js
@@ -5,23 +5,23 @@ var alreadySwitching = false;
 $(".corp-button").on("click", function() {
 	if (alreadySwitching)
 		return;
-	else
-		alreadySwitching = true;
 	
-	var str = "#" + $(this).attr('toShow');
+	var toShow = "#" + $(this).attr('toShow');
 	var ind = Number($(this).attr('index'));
-	console.log(str);
+	console.log(toShow);
 	console.log(ind);
 	
-	if (lastShown == str)
+	if (lastShown == toShow)
 		return;
+	
+	alreadySwitching = true;
 	
 	if (lastShown)
 		$(lastShown).hide("slide", {direction: (ind > lastIndex ? "left" : "right")}, 500);
 	
 	setTimeout(function() {
-		$(str).show("slide", {direction: (lastIndex ? (ind > lastIndex ? "right" : "left") : "left")}, 500, function() {
-			lastShown = str;
+		$(toShow).show("slide", {direction: (lastIndex ? (ind > lastIndex ? "right" : "left") : "left")}, 500, function() {
+			lastShown = toShow;
 			lastIndex = ind;
 			alreadySwitching = false;
 		});
